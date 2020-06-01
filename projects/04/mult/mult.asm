a9bef92cf0dbf16a8cddf5cfeda60e7204a2124f
billow <billow.fun@gmail.com>
2020-06-01 10:59:28 +0800

    multi

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// R2 = 0;
+// for(i = 0; i < R1; ++i){
+//     R2 += R0
+// }
+
+// R2 = 0; i = 0
+@R2
+M = 0
+@i
+M = 0
+
+(LOOP)
+    // i >= R1 (i-R1>=0), goto END
+    @i
+    D = M
+    @R1
+    D = D - M
+    @END
+    D;JGE
+    // i += 1
+    @i
+    M = M + 1
+    // D = R2 + R0
+    // R2 = D
+    @R2
+    D = M
+    @R0
+    D = D + M
+    @R2
+    M = D
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
